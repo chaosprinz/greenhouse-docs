cdbafbd4092d709a01b1d3429562339e078c3915
chaosprinz <chaosprinz76@googlemail.com>
2025-09-29 22:55:32 +0200

    re-implement lib/data/grows
    - ✅ responsible for getting data from db and
         throwing errors
    - ✅ returns as DbResult
    - ✅ throws NotFoundErrors
    - ✅ getGrow(id) - get a Grow by its id
    - ✅ getGrows() - get a list of all grows
    - ✅ createGrow(growData) - creates a new grow
         using given growData and returns the new
         created grow
    
    [TODO]: validation and throwing ValidationErrors

--- src/lib/data/grows.ts
+++ src/lib/data/grows.ts
@@ -1,33 +1,57 @@
 import db from "@/db";
 import { grows } from "@/db/schema";
 import { eq } from "drizzle-orm";
+import { dbResult } from "./dbResult";
+import { NotFoundError } from "./errors";
+import { Grow, GrowIncludes, GrowInput, Grows } from "./types";
 
-export async function getGrowsWithGenetic() {
-  return db.query.grows.findMany({
-    with: {
-      genetic: true,
-    },
-  });
-}
+export async function getGrows(includes?: GrowIncludes): Promise<Grows> {
+  const result = await db.query.grows.findMany({ with: includes });
 
-export async function findGrowsById(id: number) {
-  return db.query.grows.findMany({
-    where: eq(grows.id, id),
-  });
-}
-
-export async function growExists(id: number): Promise<boolean> {
-  try {
-    const grows = await findGrowsById(id);
-
-    if (!grows || grows.length <= 0) {
-      return false;
-    }
-    return true;
-  } catch {
-    return false;
+  if (result.length <= 0) {
+    throw new NotFoundError({
+      entity: "grows",
+    });
   }
+
+  return dbResult(true, result);
 }
 
-export type GrowsWithGenetic = Awaited<ReturnType<typeof getGrowsWithGenetic>>;
-export type GrowWithGenetic = GrowsWithGenetic[number];
+export async function getGrow(
+  id: number,
+  includes?: GrowIncludes
+): Promise<Grow> {
+  const result = await db.query.grows.findFirst({
+    where: eq(grows.id, id),
+    with: includes,
+  });
+
+  if (!result) {
+    throw new NotFoundError({
+      entity: "grows",
+      id,
+    });
+  }
+
+  return dbResult(true, result);
+}
+
+export async function createGrow(growInput: GrowInput): Promise<Grow> {
+  const insertId = await db
+    .insert(grows)
+    .values({ geneticId: growInput.geneticId })
+    .$returningId();
+
+  const newGrow = await db.query.grows.findFirst({
+    where: eq(grows.id, insertId[0].id),
+  });
+
+  if (!newGrow) {
+    throw new NotFoundError({
+      entity: "grows",
+      id: insertId[0].id,
+    });
+  }
+
+  return dbResult(true, newGrow);
+}
