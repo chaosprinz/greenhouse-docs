68c223ead061512c0cbd164c65523cc60da629b8
chaosprinz <chaosprinz76@googlemail.com>
2025-10-05 16:40:15 +0200

    Plug issues onto ValidationError

--- src/lib/data/errors/DbErrors/ValidationError.ts
+++ src/lib/data/errors/DbErrors/ValidationError.ts
@@ -1,13 +1,19 @@
+import { ZodIssue } from "zod/v3";
 import { DB_ERROR_CODES } from "../DB_ERROR_CODES";
 import { DbError } from "../DbError";
 import { DbErrorOptions } from "../types";
+import { $ZodIssue } from "zod/v4/core";
 
 export class ValidationError extends DbError {
-  static code = DB_ERROR_CODES.VALIDATION_ERROR;
-  code = ValidationError.code;
+  static code: string = DB_ERROR_CODES.VALIDATION_ERROR;
+  code: string = ValidationError.code;
+  issues?: $ZodIssue[];
 
   constructor(options: DbErrorOptions = {}) {
-    const message = `Validation failed for ${options.entity ?? "UnknownEntity"}.${options.field ?? "NoField"}`;
+    const message = `Validation failed for ${options.entity ?? "UnknownEntity"}`;
     super(message, options);
+    if (options.issues && options.issues.length > 0) {
+      this.issues = options.issues;
+    }
   }
 }
