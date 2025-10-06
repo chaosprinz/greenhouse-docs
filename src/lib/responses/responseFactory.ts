218fb70e3cf68b90c2fde66fbf86d7c5856fbb7b
chaosprinz <chaosprinz76@googlemail.com>
2025-10-06 16:08:57 +0200

    implement withValidation on responseFactories
    - responseFactories now have a withValidation
      property, a function returning a NextResponse
    - use zod.safeParse in withValidation to validate
      data and call the dataHandler with the validated
      data

--- src/lib/responses/responseFactory.ts
+++ src/lib/responses/responseFactory.ts
@@ -1,34 +1,105 @@
 import { NextResponse } from "next/server";
 import { createResponse } from "./createResponse";
-import { DbResult, toDbError } from "../data";
+import { DbResult, toDbError, ValidationError } from "../data";
+import { ZodSchema } from "zod";
 
-export function responseFactory<Args extends any[], T>(
-  handler: (...args: Args) => Promise<DbResult<T>>
-) {
-  const factory = async (...args: Args): Promise<NextResponse> => {
+/**
+ * # ResponseFactories
+ *
+ * What is a ResponseFactory?
+ * => A callable object, which can have a withValidation-property
+ *
+ * A ResponseFactory binds data-points to a NextResponse-creating
+ * function.
+ *
+ * withValidation is a factory-function itself, that validates data
+ * using zod and bind the validated data to a NextResponse-creating
+ * function.
+ */
+
+type ResponseFactory<Args extends any[], T> = ((
+  ...args: Args
+) => Promise<NextResponse>) & {
+  withValidation: <Input>(
+    schema: ZodSchema<Input>,
+    validateHandler: (data: Input) => Promise<DbResult<T>>
+  ) => (input: unknown) => Promise<NextResponse>;
+};
+
+/**
+ * ## createFactory(dataHandler)
+ * creates a ResponseFactory
+ * a function
+ * - which takes args and return Promise<NextResponse>
+ * - has a withValidation-property
+ */
+function createFactory<Args extends any[], T>(
+  dataHandler: (...args: Args) => Promise<DbResult<T>>
+): ResponseFactory<Args, T> {
+  const factory = async (...args: Args) => {
     try {
-      const result = await handler(...args);
-
+      const result = await dataHandler(...args);
       if (result.success) {
-        // ✅ unwrap data for success case
         return createResponse({
           success: true,
           data: result.data,
         });
       } else {
-        // ❌ handled error-case result
         return createResponse({
           success: false,
           error: result.error,
         });
       }
     } catch (err) {
-      // ⚠️ unexpected exception (not a DbResult)
       return createResponse({
         success: false,
-        error: toDbError(err as Error),
+        error: toDbError(err),
       });
     }
+  };
+
+  factory.withValidation = () => {
+    throw new Error("withValidation not implemented");
+  };
+
+  return factory;
+}
+
+/**
+ * ## responseFactory(handler)
+ * returns ResponseFactory with a withValidation-property
+ * for the given handler
+ */
+export function responseFactory<Args extends any[], T>(
+  handler: (...args: Args) => Promise<DbResult<T>>
+): ResponseFactory<Args, T> {
+  const factory = createFactory(handler);
+
+  /**
+   * ✅Adds optional Zod validation support
+   * Example usage:
+   * export const createGrowResponse =
+   *  responseFactory.withValidation(GrowSchema, createGrow)
+   */
+  factory.withValidation = function <Input>(
+    schema: ZodSchema<Input>,
+    validateHandler: (data: Input) => Promise<DbResult<T>>
+  ) {
+    return async (input: unknown): Promise<NextResponse> => {
+      const finalHandle = createFactory(validateHandler);
+      const validation = schema.safeParse(input);
+
+      if (validation.success) {
+        return finalHandle(validation.data);
+      }
+
+      return createResponse({
+        success: false,
+        error: new ValidationError({
+          issues: validation.error.issues,
+        }),
+      });
+    };
   };
 
   return factory;
